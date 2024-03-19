d7b963246c7fd2bf79faeeb1340dfcbcc8adbc7d
鳥本優奈 <yuna@yuna.local>
2024-03-19 11:58:08 +0900

    木谷竜也

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,13 +6,13 @@ type Title = {
 
 export const Header: FC<Title> = ({ title }) => {
   return (
-    <div className="grid grid-cols-8 mt-6 ">
+    <div className="grid grid-cols-8 mt-4 ">
       <img
         src="Icon.png"
         alt="app_icon"
         className="grid col-start-2 col-end-4 ml-2 w-[100px] h-[100px] justify-center content-center round rounded-full"
       />
-      <h1 className="grid text-2xl col-start-4 col-end-8 justify-center content-center">
+      <h1 className="grid text-3xl col-start-4 col-end-8 justify-center content-center">
         {title}
       </h1>
     </div>
